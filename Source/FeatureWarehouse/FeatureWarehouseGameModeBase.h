0bc094aa9ad330f3cf4ffe606fb73dee37a20873
Move <movesky98@naver.com>
2023-12-22 11:39:37 +0900

    Project Setup

---
+++ Source/FeatureWarehouse/FeatureWarehouseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FeatureWarehouseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FEATUREWAREHOUSE_API AFeatureWarehouseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
